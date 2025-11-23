1249e797be846764f537d3b3251c18779070855e
kjbrian9 <111594410+kjbrian9@users.noreply.github.com>
2025-11-23 22:14:23 +0100

    changed 2xl responsivity on second section

--- src/app/components/SecondSection.js
+++ src/app/components/SecondSection.js
@@ -5,10 +5,10 @@ function SecondSection() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center w-full mb-20 ">
-      <div className="flex flex-col xl:flex-row gap-5 2xl:gap-30 items-center justify-center">
+      <div className="flex flex-col xl:flex-row gap-5 2xl:gap-20 items-center justify-center">
         <img
           src="/CoupleOne.jpg"
-          className="w-80 lg:w-120 2xl:w-160 rounded-xl hover:scale-105 duration-300"
+          className="w-80 lg:w-120 2xl:w-150 rounded-xl hover:scale-105 duration-300"
           alt="Couple image 1"
         />
         <div className="lg:w-180 text-black text-center xl:text-start flex flex-col items-center xl:items-start justify-center">
@@ -21,12 +21,12 @@ function SecondSection() {
           <div className="flex gap-5 items-center justify-center">
             <img
               src="/CoupleTwo.jpg"
-              className="w-40 lg:w-80 2xl:w-110 rounded-xl hover:scale-105 duration-300"
+              className="w-40 lg:w-80 2xl:w-100 rounded-xl hover:scale-105 duration-300"
               alt="Couple image 2"
             />
             <img
               src="/CoupleThree.jpg"
-              className="w-40 lg:w-80 2xl:w-110 rounded-xl hover:scale-105 duration-300"
+              className="w-40 lg:w-80 2xl:w-100 rounded-xl hover:scale-105 duration-300"
               alt="Couple image 3"
             />
           </div>
